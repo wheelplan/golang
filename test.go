6af448502ea295c20c188faf4aeb7e87ffe83d39
alan <58475947+yuiyar@users.noreply.github.com>
2020-11-11 09:18:49 +0800

    .

--- test.go
+++ test.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"reflect"
 )
 
-type str string
-
-func test(c str) {
-	fmt.Println(c)
-}
-
 func main() {
-	const a = 0x200
-	fmt.Println(reflect.TypeOf(a), a, 256*3)
+	const a = 0x100
+	fmt.Println(reflect.TypeOf(a), a, 16*16)
+
+	const b = 0100
+	fmt.Println(reflect.TypeOf(b), b, 8*8)
+
+	const c = 100
+	fmt.Println(reflect.TypeOf(c), c, 10*10)
+
+	fmt.Printf("0b%b, %#o, %#x\n", c, b, a)
+
+	fmt.Println(math.MinInt8, math.MaxInt8)
 }
