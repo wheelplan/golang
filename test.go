00fc2c04da77c7a2d9acc1c23e1ff6f70bde3c92
alan <58475947+yuiyar@users.noreply.github.com>
2020-11-16 09:10:05 +0800

    .

--- test.go
+++ test.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	/*	const a = 0x100
@@ -17,21 +15,12 @@ func main() {
 
 		fmt.Println(math.MinInt8, math.MaxInt8)*/
 
-	var b int = 1
-	var c int64 = int64(b)
-	fmt.Println(c)
+	type data struct {
+		x int
+		y bool
+	}
 
-	x := 100
-	p := (*int)(&x)
-	fmt.Println(*p)
+	var z data = data{1, true}
 
-	type flags byte
-	const (
-		read flags = 3 << iota
-		write
-		exec
-	)
-	f := 2 | 5
-	fmt.Printf("%b && %b && %b\n", read, write, exec)
-	fmt.Printf("%b && %v\n", f, f)
+	fmt.Println(z)
 }
