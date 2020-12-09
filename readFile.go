c19a8d9c01342d2a175f7d0e81660743d62899f9
alan <58475947+yuiyar@users.noreply.github.com>
2020-12-09 10:17:05 +0800

    .

--- readFile.go
+++ readFile.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -10,8 +11,9 @@ import (
 func main() {
 
 	//readOne("C:\\GoProgram\\src\\tt\\goNotes\\readFile.go")
-	readTwo("C:\\GoProgram\\src\\tt\\goNotes\\readFile.go")
-
+	//readTwo("C:\\GoProgram\\src\\tt\\goNotes\\readFile.go")
+	//readThree("C:\\GoProgram\\src\\tt\\goNotes\\readFile.go")
+	readFour("C:\\GoProgram\\src\\tt\\goNotes\\readFile.go")
 }
 
 func readOne(s string) {
@@ -48,4 +50,49 @@ func readTwo(s string) {
 		panic("Open file failed.")
 	}
 	fmt.Println(string(f))
+}
+
+func readThree(s string) {
+	f, err := os.Open(s)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+
+	var buf [128]byte
+	var tmp []byte
+
+	for {
+		n, err := r.Read(buf[:])
+		if err != nil && err != io.EOF {
+			panic("Read file failed.")
+		}
+
+		if n == 0 {
+			break
+		}
+
+		tmp = append(tmp, buf[:n]...)
+	}
+
+	fmt.Println(string(tmp))
+}
+
+func readFour(s string) {
+	f, err := os.Open(s)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	file, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic("Read file failed.")
+	}
+
+	fmt.Println(string(file))
 }
