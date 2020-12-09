38edbe15f5e47ff2a5edde7de9437a685357f467
alan <58475947+yuiyar@users.noreply.github.com>
2020-12-09 14:44:10 +0800

    .

--- writeFile.go
+++ writeFile.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
-	writeOne("./writeFileTest.go")
+	//writeOne("./writeFileTest.go")
+	//writeTwo("./writeFileTest.go")
+	//writeThree("./writeFileTest.go")
+	writeFour("./writeFileTest.go")
 }
 
 func checkFileExist(filename string) bool {
@@ -18,9 +23,10 @@ func checkFileExist(filename string) bool {
 	return true
 }
 
+// io.WriteString
 func writeOne(s string) {
 
-	content := "Are you okay .\n"
+	content := "Are you okay 1.\n"
 
 	if !checkFileExist(s) {
 		_, err := os.Create(s)
@@ -36,6 +42,8 @@ func writeOne(s string) {
 		return
 	}
 
+	defer f.Close()
+
 	n, err := io.WriteString(f, content)
 	if err != nil {
 		fmt.Println("write file failed", err)
@@ -43,4 +51,71 @@ func writeOne(s string) {
 	}
 
 	fmt.Println("write byte", n)
+}
+
+// ioutil.WriteFile
+func writeTwo(s string) {
+	content := "Are you okay 2.\n"
+	var buf = []byte(content)
+
+	err := ioutil.WriteFile(s, buf, 0666)
+	if err != nil {
+		fmt.Println("write file failed", err)
+		return
+	}
+
+	fmt.Println("write success .")
+}
+
+// File(Write,WriteString)
+func writeThree(s string) {
+	content := "Are you okay 3.\n"
+	var buf = []byte(content)
+
+	var f *os.File
+
+	if !checkFileExist(s) {
+		_, err := os.Create(s)
+		if err != nil {
+			fmt.Println("file create failed", err)
+			return
+		}
+	}
+
+	f, err := os.OpenFile(s, os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("file open failed", err)
+		return
+	}
+
+	defer f.Close()
+
+	n1, err1 := f.Write(buf)
+	if err1 != nil {
+		fmt.Println("write err", err1)
+	}
+	fmt.Println("write byte", n1)
+
+	n2, err2 := f.WriteString(content)
+	if err2 != nil {
+		fmt.Println("write err", err2)
+	}
+	fmt.Println("write byte", n2)
+}
+
+// bufio.NewWriter
+func writeFour(s string) {
+	content := "Are you okay 4.\n"
+
+	f, err := os.Create(s)
+	if err != nil {
+		fmt.Println("file create failed", err)
+		return
+	}
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	n, err := w.WriteString(content)
+	fmt.Println("write byte", n)
+	w.Flush()
+
 }
