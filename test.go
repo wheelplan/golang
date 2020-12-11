0ccbb0b85db9ab2ef7d3bacac507dfa6d66315d6
alan <yuiya@yahoo.com>
2020-12-11 09:08:02 +0800

    .

---
+++ test.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type animal interface {
+	move()
+	eat()
+}
+
+type cat struct {
+	name string
+	feet int8
+}
+
+type chicken struct {
+	feet int8
+}
+
+func main() {
+}
+
