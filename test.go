9111e46d6623a4260d7e2b20ff58302458309a8a
alan <58475947+yuiyar@users.noreply.github.com>
2020-12-14 16:48:17 +0800

    .

--- test.go
+++ test.go
@@ -2,22 +2,56 @@ package main
 
 import "fmt"
 
-func twoSum(nums []int, target int) []int {
-
-	for i, _ := range nums {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
-		}
-	}
-	return nil
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
 func main() {
+	a1 := ListNode{7, nil}
+	a2 := ListNode{1, &a1}
+	a3 := ListNode{6, &a2}
 
-	var nums []int = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	var target int = 7
-	fmt.Println(twoSum(nums, target))
+	b1 := ListNode{5, nil}
+	b2 := ListNode{1, &b1}
+	b3 := ListNode{7, &b2}
 
+	res := addTwoNumbers(&a3, &b3)
+	fmt.Println(res.Val, res.Next.Val, res.Next.Next.Val, res.Next.Next.Next.Val)
+}
+
+func addTwoNumbers(l1, l2 *ListNode) (head *ListNode) {
+	var tail *ListNode
+	var carry int
+
+	for l1 != nil || l2 != nil {
+		var a, b int
+
+		if l1 != nil {
+			a = l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			b = l2.Val
+			l2 = l2.Next
+		}
+
+		sum := a + b + carry
+		sum, carry = sum%10, sum/10
+
+		if head == nil {
+			head = &ListNode{Val: sum}
+			tail = head
+		} else {
+			tail.Next = &ListNode{Val: sum}
+			tail = tail.Next
+		}
+	}
+
+	if carry > 0 {
+		tail.Next = &ListNode{Val: carry}
+	}
+
+	return
 }
