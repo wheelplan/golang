4ebf14664077a1055f1f7510f38b94ff077b0390
alan <58475947+yuiyar@users.noreply.github.com>
2020-12-14 16:54:21 +0800

    .

--- leetcodeTest/1. 两数之和.go
+++ leetcodeTest/1. 两数之和.go
@@ -1,7 +1,3 @@
-package main
-
-import "fmt"
-
 /*给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
@@ -12,6 +8,10 @@ import "fmt"
 
 因为 nums[0] + nums[1] = 2 + 7 = 9
 所以返回 [0, 1]*/
+
+package main
+
+import "fmt"
 
 func main() {
 	fmt.Println(twoSum([]int{0, 1, 6, 7, 9}, 6))
