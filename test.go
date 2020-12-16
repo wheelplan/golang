0bfb687ced508a2e8b02340b76c209294a6c0ed7
alan <58475947+yuiyar@users.noreply.github.com>
2020-12-16 13:32:29 +0800

    .

--- test.go
+++ test.go
@@ -1,62 +1,41 @@
-//给定一个非负整数 N，找出小于或等于 N 的最大的整数，同时这个整数需要满足其各个位数上的数字是单调递增。
-//
-//（当且仅当每个相邻位数上的数字 x 和 y 满足 x <= y 时，我们称这个整数是单调递增的。）
-//
-//示例 1:
-//
-//输入: N = 10
-//输出: 9
-//示例 2:
-//
-//输入: N = 1234
-//输出: 1234
-//示例 3:
-//
-//输入: N = 332
-//输出: 299
-//说明: N 是在 [0, 10^9] 范围内的一个整数。
-
 package main
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 )
 
 func main() {
-	res := monotoneIncreasingDigits(221)
-	fmt.Println(res)
+
+	pattern := "absba"
+	s := "cat dog dog dog cat"
+
+	fmt.Println(wordPattern(pattern, s))
+
 }
 
-func monotoneIncreasingDigits(n int) int {
-	s := []byte(strconv.Itoa(n))
+func wordPattern(pattern string, s string) bool {
+	m := make(map[byte]string)
 
-	for i := len(s) - 1; i > 0; i-- {
-		if s[i] < s[i-1] {
-			for b := i; b < len(s); b++ {
-				s[b] = '9'
-			}
-			s[i-1] -= 1
-		}
+	words := strings.Split(s, " ")
+	if len(pattern) != len(words) {
+		return false
 	}
 
-	res, _ := strconv.Atoi(string(s))
-	return res
+	for i := 0; i < len(words); i++ {
+
+		if m[pattern[i]] == "" {
+			m[pattern[i]] = words[i]
+			for j := i - 1; j >= 0; j-- {
+				if words[i] == words[j] {
+					return false
+				}
+			}
+		} else {
+			if m[pattern[i]] != words[i] {
+				return false
+			}
+		}
+	}
+	return true
 }
-
-/*func monotoneIncreasingDigits(n int) int {
-	s := []byte(strconv.Itoa(n))
-
-	for i := len(s) - 1; i > 0; i-- {
-		if s[i] < s [i-1] {
-			for b := i; b < len(s); b++ {
-				s[b] = '9'
-			}
-			s[i-1] -= 1
-		}
-	}
-
-	res, _ := strconv.Atoi(string(s))
-
-	return res
-}*/
