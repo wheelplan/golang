60bb0340be6e72cb61a78f27977e47ab72011375
Alan <yuiya@yahoo.com>
2021-01-21 11:27:05 +0800

    .

--- notes/test.go
+++ notes/test.go
@@ -2,26 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
 )
 
-func init() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: ./test.go <url>")
-		os.Exit(-1)
-	}
-}
 func main() {
-	r, err := http.Get(os.Args[1])
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	p := []byte("Golang")
 
-	io.Copy(os.Stdout, r.Body)
-	if err := r.Body.Close(); err != nil {
-		fmt.Println(err)
-	}
+	fmt.Println(p)
 }
