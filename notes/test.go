bc3277be4ac3dbb1db4b8eb4604946fcd63beaa0
alan <58475947+yuiyar@users.noreply.github.com>
2021-01-26 14:52:41 +0800

    .

--- notes/test.go
+++ notes/test.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
+	counts := make(map[string]int)
 
-	sli := []int{1, 2, 3, 4, 5, 6, 7}
+	files := os.Args[1:]
 
-	s := sli[1:7]
+	if len(files) == 0 {
+		countlines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Println("dup2: ", os.Stderr, err)
+				continue
+			}
+			countlines(f, counts)
+			f.Close()
+		}
+	}
 
-	fmt.Println("s: ", s)
-	fmt.Println("length: ", len(s))
-	fmt.Println("capacity: ", cap(s))
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Println(n, line)
+		}
+	}
+	// test
+}
 
+func countlines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// test
 }
