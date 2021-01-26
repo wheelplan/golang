bcf9bedd57dac8e754da979152496e9c40f1d7f0
alan <58475947+yuiyar@users.noreply.github.com>
2021-01-26 16:24:55 +0800

    .

--- notes/test.go
+++ notes/test.go
@@ -1,32 +1,55 @@
+// Lissajous generates GIF animations of random Lissajous figures.
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
 	"os"
-	"strings"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
 )
 
 func main() {
+	// The sequence of image is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	rand.Seed(time.Now().UTC().UnixNano())
+	lissajous(os.Stdout)
+}
 
-	counts := make(map[string]int)
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
 
-	for _, filename := range os.Args[1:] {
-		data, err := ioutil.ReadFile(filename)
-		if err != nil {
-			fmt.Println("read file failed", err)
-			continue
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
 		}
-
-		for _, line := range strings.Split(string(data), "\n") {
-			counts[line]++
-		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
-
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Println(n, line)
-		}
-
-	}
+	gif.EncodeAll(out, &anim)
 }
