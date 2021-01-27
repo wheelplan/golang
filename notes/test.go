14d54579125bafe5b511b1d769ccb29f8c4447fa
alan <58475947+yuiyar@users.noreply.github.com>
2021-01-27 15:53:06 +0800

    .

--- notes/test.go
+++ notes/test.go
@@ -1,55 +1,44 @@
-// Lissajous generates GIF animations of random Lissajous figures.
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/gif"
-	"io"
-	"math"
-	"math/rand"
+	"fmt"
 	"os"
+	"syscall"
 	"time"
 )
 
-var palette = []color.Color{color.White, color.Black}
-
-const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
-)
-
 func main() {
-	// The sequence of image is deterministic unless we seed
-	// the pseudo-random number generator using the current time.
-	rand.Seed(time.Now().UTC().UnixNano())
-	lissajous(os.Stdout)
-}
 
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)
-
-	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // phase difference
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
-		}
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
+	filename := "./doc/poem.txt"
+	file, err := os.OpenFile(filename, os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Println(err)
 	}
-	gif.EncodeAll(out, &anim)
+
+	buf := []byte{'1', '2', '3', '5', ' ', 'a', 'l', 'a', 'n'}
+	var tmp []byte
+
+	file.Seek(0, os.SEEK_END)
+	n, err := file.Write(buf[:])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tmp = append(tmp, buf[:n]...)
+
+	fmt.Println(string(tmp))
+
+	fmt.Println("end")
+	file.Seek(-4, os.SEEK_END)
+	file.Truncate(1)
+	a, err := os.Stat(filename)
+	fmt.Println(a.Name())
+	info := a.Sys().(*syscall.StartupInfo)
+	fmt.Println(info)
+
+	error := os.Chtimes(filename, time.Now(), time.Now())
+	if err != nil {
+		fmt.Println(error)
+	}
+
 }
