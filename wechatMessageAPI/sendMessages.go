2f942966a7130eab9d27ec51882c48e11680fbc1
alan <58475947+yuiyar@users.noreply.github.com>
2021-01-28 17:41:15 +0800

    .

--- wechatMessageAPI/sendMessages.go
+++ wechatMessageAPI/sendMessages.go
@@ -10,21 +10,26 @@ import (
 )
 
 func main() {
+
 	corpid := "wwd327d0ea50c0dae0"
 	appsecret := "1kJw7t38aUxb3doG7IaLGVwOsDEuDHOMFUO2Z0xa_lI"
 	agentid := 1000008
 	toparty := os.Args[1]
+
 	accessToken := GetToken(corpid, appsecret)
 	SendMSG(accessToken, agentid, toparty)
+
 }
 
 func GetToken(corpid, appsecret string) string {
+
 	tokenURL := "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + corpid + "&corpsecret=" + appsecret
 
 	r, err := http.Get(tokenURL)
 	if err != nil {
 		fmt.Println("Get Token http.Get ERROR ! ", err)
 	}
+
 	defer r.Body.Close()
 
 	body, err := ioutil.ReadAll(r.Body)
@@ -49,7 +54,9 @@ func GetToken(corpid, appsecret string) string {
 }
 
 func SendMSG(accessToken string, agentid int, toparty string) {
+
 	sendURL := "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + accessToken
+
 	var message string
 	for _, arg := range os.Args[2:] {
 		message += arg + "\n"
@@ -77,24 +84,18 @@ func SendMSG(accessToken string, agentid int, toparty string) {
 
 	paramsJSON, _ := json.Marshal(params)
 
-	req, err := http.NewRequest("POST", sendURL, bytes.NewBuffer(paramsJSON))
+	req, err := http.Post(sendURL, "application/json", bytes.NewBuffer(paramsJSON))
 	if err != nil {
 		fmt.Println("SendMSG HTTP POST ERR ! ", err)
 	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("charset", "UTF-8")
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	errCode := resp.Header["Error-Code"][0]
+	defer req.Body.Close()
+
+	errCode := req.Header["Error-Code"][0]
 	if errCode != "0" {
-		fmt.Println("status:", resp.Status)
-		fmt.Println("response Header:", resp.Header)
-		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println("status:", req.Status)
+		fmt.Println("response Header:", req.Header)
+		body, _ := ioutil.ReadAll(req.Body)
 		fmt.Println("response Body:", string(body))
 	}
 }
