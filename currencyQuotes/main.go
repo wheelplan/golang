f9db3949a140328ad371959299546b847dc8957f
alan <58475947+yuiyar@users.noreply.github.com>
2021-02-03 11:21:43 +0800

    .

--- currencyQuotes/main.go
+++ currencyQuotes/main.go
@@ -1,40 +1,42 @@
 package main
 
 import (
+	"golang/currencyQuotes/mysql"
 	"golang/currencyQuotes/price"
 	"log"
 	"time"
 
-	"golang/currencyQuotes/mysql"
 	//	"golang/currencyQuotes/sms"
 	"golang/currencyQuotes/wechat"
 )
 
 func main() {
 
-	expectedPrice := 1520.00
+	coinPrice := map[string]float64{
+		"btcusdt":  0.00,
+		"ethusdt":  0.00,
+		"dogeusdt": 0.00,
+	}
+	expectedPrice := map[string]float64{
+		"btcusdt":  37000.00,
+		"ethusdt":  1520.00,
+		"dogeusdt": 0.034,
+	}
 
 	for {
-		coinPrice := map[string]float64{
-			"btcusdt":  0.00,
-			"ethusdt":  0.00,
-			"dogeusdt": 0.00,
-		}
-
 		price.GetCoinPrice(coinPrice)
 
 		for k, v := range coinPrice {
 
-			if v > expectedPrice {
+			if v > expectedPrice[k] {
 				wechat.Send(k, v)
-				expectedPrice *= 1.01
+				expectedPrice[k] *= 1.01
 			}
 
 			log.Println(k, ": $", v)
 			mysql.CoinMySQLData(k, v)
-			time.Sleep(time.Duration(6) * time.Second)
-
 		}
 
+		time.Sleep(time.Duration(6) * time.Second)
 	}
 }
