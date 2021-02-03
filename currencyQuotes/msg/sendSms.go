ef668aae7d0bf1921b9e406327a3b480565a7261
alan <58475947+yuiyar@users.noreply.github.com>
2021-02-03 18:00:10 +0800

    .

---
+++ currencyQuotes/msg/sendSms.go
@@ -0,0 +1,79 @@
+package msg
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+	sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20190711"
+	"strconv"
+)
+
+func Send(currency string, coinPrice float64) {
+	credential := common.NewCredential(
+		"AKIDHbr1auq2oQx61pJ8ze4IH2YqujKi8Or7",
+		"l7PPBi16vQffqojS9cr0tZ7RdTLyMdbL",
+	)
+	/* 非必要步骤:
+	 * 实例化一个客户端配置对象，可以指定超时时间等配置 */
+	cpf := profile.NewClientProfile()
+
+	/* SDK默认使用POST方法。
+	 * 如果你一定要使用GET方法，可以在这里设置。GET方法无法处理一些较大的请求 */
+	cpf.HttpProfile.ReqMethod = "POST"
+
+	/* SDK有默认的超时时间，非必要请不要进行调整
+	 * 如有需要请在代码中查阅以获取最新的默认值 */
+	//cpf.HttpProfile.ReqTimeout = 5
+
+	/* SDK 会自动指定域名。通常是不需要特地指定域名的，但是如果你访问的是金融区的服务
+	 * 则必须手动指定域名，例如 sms 的上海金融区域名： sms.ap-shanghai-fsi.tencentcloudapi.com */
+	cpf.HttpProfile.Endpoint = "sms.tencentcloudapi.com"
+
+	/* SDK默认用TC3-HMAC-SHA256进行签名，非必要请不要修改这个字段 */
+	cpf.SignMethod = "HmacSHA1"
+
+	/* 实例化要请求产品(以sms为例)的client对象
+	 * 第二个参数是地域信息，可以直接填写字符串ap-guangzhou，或者引用预设的常量 */
+	client, _ := sms.NewClient(credential, "ap-guangzhou", cpf)
+
+	/* 实例化一个请求对象，根据调用的接口和实际情况，可以进一步设置请求参数
+	* 你可以直接查询SDK源码确定接口有哪些属性可以设置
+	 * 属性可能是基本类型，也可能引用了另一个数据结构
+	 * 推荐使用IDE进行开发，可以方便的跳转查阅各个接口和数据结构的文档说明 */
+	request := sms.NewSendSmsRequest()
+
+	/* 短信应用ID: 短信SdkAppid在 [短信控制台] 添加应用后生成的实际SdkAppid，示例如1400006666 */
+	request.SmsSdkAppid = common.StringPtr("1400482933")
+	/* 短信签名内容: 使用 UTF-8 编码，必须填写已审核通过的签名，签名信息可登录 [短信控制台] 查看 */
+	request.Sign = common.StringPtr("生活中的智慧")
+	/* 国际/港澳台短信 senderid: 国内短信填空，默认未开通，如需开通请联系 [sms helper] */
+	request.SenderId = common.StringPtr("")
+	/* 用户的 session 内容: 可以携带用户侧 ID 等上下文信息，server 会原样返回 */
+	request.SessionContext = common.StringPtr("")
+	/* 短信码号扩展号: 默认未开通，如需开通请联系 [sms helper] */
+	request.ExtendCode = common.StringPtr("0")
+	/* 模板参数: 若无模板参数，则设置为空*/
+	request.TemplateParamSet = common.StringPtrs([]string{currency + " " + strconv.FormatFloat(coinPrice, 'f', 2, 64)})
+	/* 模板 ID: 必须填写已审核通过的模板 ID。模板ID可登录 [短信控制台] 查看 */
+	request.TemplateID = common.StringPtr("861930")
+	/* 下发手机号码，采用 e.164 标准，+[国家或地区码][手机号]
+	 * 示例如：+8613711112222， 其中前面有一个+号 ，86为国家码，13711112222为手机号，最多不要超过200个手机号*/
+	request.PhoneNumberSet = common.StringPtrs([]string{"+8615549494959"})
+
+	// 通过client对象调用想要访问的接口，需要传入请求对象
+	response, err := client.SendSms(request)
+	// 处理异常
+	if _, ok := err.(*errors.TencentCloudSDKError); ok {
+		fmt.Printf("An API error has returned: %s", err)
+		return
+	}
+	// 非SDK异常，直接失败。实际代码中可以加入其他的处理。
+	if err != nil {
+		panic(err)
+	}
+	b, _ := json.Marshal(response.Response)
+	// 打印返回的json字符串
+	fmt.Printf("%s", b)
+}
