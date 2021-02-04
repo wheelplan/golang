d68c9088e3df5647384a7cb9ee4e19bc64e337a6
alan <58475947+yuiyar@users.noreply.github.com>
2021-02-04 14:25:37 +0800

    .

--- notes/test.go
+++ notes/test.go
@@ -1,41 +1,67 @@
-// Fetchall fetches URLs in parallel and reports their times and sizes.
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	ch := make(chan string)
-	for _, url := range os.Args[1:] {
-		go fetch(url, ch) // start a goroutine
+	Price := map[string]float64{
+		"btcusdt":  0.00,
+		"ethusdt":  0.00,
+		"dogeusdt": 0.00,
 	}
-	<-ch
-	//for range os.Args[1:] {
-	//	fmt.Println(<-ch) // receive from channel ch
-	//}
+	ch := make(chan string)
+	for currency, _ := range Price {
+		priceAPI := "https://api.huobi.pro/market/trade?symbol=" + currency
+		go fetch(priceAPI, ch)
+	}
+	for range Price {
+		fmt.Println(<-ch)
+	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
-func fetch(url string, ch chan<- string) {
+func fetch(url string, ch chan<- string) float64 {
 	start := time.Now()
-	resp, err := http.Get(url)
+
+	r, err := http.Get(url)
 	if err != nil {
-		ch <- fmt.Sprint(err) // send to channel ch
-		return
+		log.Println("Get Coin Price http.Get ERROR ! ", err)
 	}
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
-	resp.Body.Close() // don't leak resources
-	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
-		return
+
+	defer r.Body.Close()
+	type coinPrice struct {
+		Ch     string `json:"ch"`
+		Status string `json:"status"`
+		//	Ts     time.Time `json:"ts"`
+		Tick struct {
+			ID int `json:"id"`
+			//	Ts1  time.Time `json:"ts"`
+			Data []struct {
+				//Id int `json:"id"`
+				//Ts2       time.Time `json:"ts"`
+				//TradeId int `json:"trade-id"`
+				//Amount    float64 `json:"amount"`
+				Price     float64 `json:"price"`
+				Direction string  `json:"direction"`
+			} `json:"data"`
+		} `json:"tick"`
 	}
+
+	var coinprice coinPrice
+	jsonERR := json.NewDecoder(r.Body).Decode(&coinprice)
+	if jsonERR != nil {
+		log.Fatalln("Get Coin Price json.NewDecoder.Decode ERR ! ", jsonERR)
+	}
+
+	// return coinprice.Tick.Data[0].Price
 	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, coinprice.Tick.Data[0].Price, url)
+
+	return coinprice.Tick.Data[0].Price
 }
