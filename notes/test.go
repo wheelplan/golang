b9cdda997c8eb540ca03d31e187403d2878c839f
alan <58475947+yuiyar@users.noreply.github.com>
2021-03-10 12:58:45 +0800

    .

--- notes/test.go
+++ notes/test.go
@@ -2,67 +2,64 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
 func main() {
-	start := time.Now()
-	Price := map[string]float64{
+	coinPrice := map[string]float64{
 		"btcusdt":  0.00,
 		"ethusdt":  0.00,
 		"dogeusdt": 0.00,
 	}
-	ch := make(chan string)
-	for currency, _ := range Price {
-		priceAPI := "https://api.huobi.pro/market/trade?symbol=" + currency
-		Price[currency] = fetch(priceAPI, ch)
-		fmt.Println(<-ch)
-
-	}
-
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
-	fmt.Println(Price)
+	GetCoinPrice(coinPrice)
 }
 
-func fetch(url string, ch chan<- string) float64 {
+func GetCoinPrice(Price map[string]float64) {
 	start := time.Now()
+	ch := make(chan float64, 3)
+	var wg sync.WaitGroup
 
-	r, err := http.Get(url)
-	if err != nil {
-		log.Println("Get Coin Price http.Get ERROR ! ", err)
+	for k, _ := range Price {
+		wg.Add()
+		priceAPI := "https://api.huobi.pro/market/trade?symbol=" + k
+
+		r, err := http.Get(priceAPI)
+		if err != nil {
+			log.Println("Get Coin Price http.Get ERROR ! ", err)
+			continue
+		}
+
+		defer r.Body.Close()
+		type coinPrice struct {
+			Ch     string `json:"ch"`
+			Status string `json:"status"`
+			//	Ts     time.Time `json:"ts"`
+			Tick struct {
+				ID int `json:"id"`
+				//	Ts1  time.Time `json:"ts"`
+				Data []struct {
+					//Id int `json:"id"`
+					//Ts2       time.Time `json:"ts"`
+					//TradeId int `json:"trade-id"`
+					//Amount    float64 `json:"amount"`
+					Price     float64 `json:"price"`
+					Direction string  `json:"direction"`
+				} `json:"data"`
+			} `json:"tick"`
+		}
+
+		var coinprice coinPrice
+		jsonERR := json.NewDecoder(r.Body).Decode(&coinprice)
+		if jsonERR != nil {
+			log.Fatalln("Get Coin Price json.NewDecoder.Decode ERR ! ", jsonERR)
+		}
+
+		// return coinprice.Tick.Data[0].Price
+		Price[k] = coinprice.Tick.Data[0].Price
 	}
-
-	defer r.Body.Close()
-	type coinPrice struct {
-		Ch     string `json:"ch"`
-		Status string `json:"status"`
-		//	Ts     time.Time `json:"ts"`
-		Tick struct {
-			ID int `json:"id"`
-			//	Ts1  time.Time `json:"ts"`
-			Data []struct {
-				//Id int `json:"id"`
-				//Ts2       time.Time `json:"ts"`
-				//TradeId int `json:"trade-id"`
-				//Amount    float64 `json:"amount"`
-				Price     float64 `json:"price"`
-				Direction string  `json:"direction"`
-			} `json:"data"`
-		} `json:"tick"`
-	}
-
-	var coinprice coinPrice
-	jsonERR := json.NewDecoder(r.Body).Decode(&coinprice)
-	if jsonERR != nil {
-		log.Fatalln("Get Coin Price json.NewDecoder.Decode ERR ! ", jsonERR)
-	}
-
-	// return coinprice.Tick.Data[0].Price
 	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs  %8.4f  %s", secs, coinprice.Tick.Data[0].Price, url)
-
-	return coinprice.Tick.Data[0].Price
+	log.Println(secs)
 }
