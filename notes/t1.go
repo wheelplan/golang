1719e627ede320d005a1f524c5c66833bceb0a0c
alan <58475947+yuiyar@users.noreply.github.com>
2021-03-10 13:02:15 +0800

    .

---
+++ notes/t1.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	SecretKey = "8a03c923-805388e0-b57aa7b0-af1ce"
+)
+
+func main() {
+	AccessKey := "ffde1a6e-31848ea0-45e77dbf-mn8ikls4qg"
+	timestamp := time.Now().Format("2006-01-02T15:04:05")
+
+	message := "GET\n" + "api.huobi.pro\n" + "/v1/order/orders\n" +
+		"AccessKeyId=" + AccessKey +
+		"&SignatureMethod=HmacSHA256" +
+		"&SignatureVersion=2" +
+		"Timestamp=" + timestamp
+
+	r, err := http.Get("https://api.huobi.pro/v1/order/orders?AccessKeyId=" + AccessKey +
+		"&SignatureMethod=HmacSHA256" +
+		"&SignatureVersion=2" +
+		"Timestamp=" + timestamp +
+		"&Signature=" + ComputeHmacSha256(message, SecretKey))
+	if err != nil {
+		log.Println("Get Coin Price http.Get ERROR ! ", err)
+	}
+	defer r.Body.Close()
+	fmt.Println(r.Body)
+}
+
+func ComputeHmacSha256(message, secret string) string {
+
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	return base64.URLEncoding.EncodeToString(h.Sum(nil))
+}
