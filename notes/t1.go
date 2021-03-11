2ec7f05e63c9abed8ba2bdba8edbdcc0b84d0996
yuiya <yuiya@yahoo.com>
2021-03-11 15:43:35 +0800

    .

--- notes/t1.go
+++ notes/t1.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
+	"net/url"
+	"os"
+	"sort"
 	"time"
 )
 
@@ -15,31 +19,51 @@ const (
 )
 
 func main() {
-	AccessKey := "ffde1a6e-31848ea0-45e77dbf-mn8ikls4qg"
-	timestamp := time.Now().Format("2006-01-02T15:04:05")
 
-	message := "GET\n" + "api.huobi.pro\n" + "/v2/account/asset-valuation\n" +
-		"AccessKeyId=" + AccessKey +
-		"&SignatureMethod=HmacSHA256" +
-		"&SignatureVersion=2" +
-		"Timestamp=" + timestamp
-
-	r, err := http.Get("https://api.huobi.pro/v2/account/asset-valuation?AccessKeyId=" + AccessKey +
-		"&SignatureMethod=HmacSHA256" +
-		"&SignatureVersion=2" +
-		"Timestamp=" + timestamp +
-		"&Signature=" + ComputeHmacSha256(message, SecretKey))
-	if err != nil {
-		log.Println("Get Coin Price http.Get ERROR ! ", err)
+	params := map[string]string {
+		"SignatureMethod":	"HmacSHA256",
+		"SignatureVersion":	"2",
+		"Timestamp":     	time.Now().UTC().Format("2006-01-02T15:04:05"),
+		"AccessKeyId":   	"ffde1a6e-31848ea0-45e77dbf-mn8ikls4qg",
+		"accountType":		"spot",
+		"valuationCurrency": 	"CNY",
 	}
-	defer r.Body.Close()
-	fmt.Println(r.Body)
-}
 
-func ComputeHmacSha256(message, secret string) string {
 
-	key := []byte(secret)
-	h := hmac.New(sha256.New, key)
-	h.Write([]byte(message))
-	return base64.URLEncoding.EncodeToString(h.Sum(nil))
+	var buf bytes.Buffer
+	a := "GET\n" + "api.huobi.pro\n" + "/v2/account/asset-valuation\n"
+
+	keys := make([]string, 0, len(params))
+	for k, _ := range params {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for i := range keys {
+		k := keys[i]
+		buf.WriteString(k)
+		buf.WriteString("=")
+		buf.WriteString(url.QueryEscape(params[k]))
+		buf.WriteString("&")
+	}
+	buf.Truncate(buf.Len() - 1)
+
+	hashed := hmac.New(sha256.New, []byte(SecretKey))
+	var data []byte = []byte(a)
+	hashed.Write(append(data, buf.Bytes()...))
+
+	r, err := http.Get("https://api.huobi.pro/v2/account/asset-valuation?" +
+		string(buf.Bytes()) +
+		"&Signature=" +
+		url.QueryEscape(base64.StdEncoding.EncodeToString(hashed.Sum(nil))))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	io.Copy(os.Stdout, r.Body)
+	if err := r.Body.Close(); err != nil {
+		fmt.Println(err)
+	}
 }
